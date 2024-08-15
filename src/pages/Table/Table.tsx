420e4345ea2527cf6123ad1050a3974f1969d9e6
neytor <ortegajoseweb02@gmail.com>
2024-08-15 08:57:48 -0300

    a

--- src/pages/Table/Table.tsx
+++ src/pages/Table/Table.tsx
@@ -9,28 +9,7 @@ interface TableData {
     items: string[];
   }
 function Table() {
-  
     const [tables,setTables] =useState<Array<TableData>>([])
-    // const fetchTables= async()=>{
-    //     try{
-    //         const response =await fetch(urlBackend+"/table",{method:"GET",credentials:"include"})
-    //         if(response.ok){
-    //             const result = await response.json();
-    //             setTables(result)
-    //         }else{
-    //             console.log("failde to fetch")
-    //         }
-            
-    //     }catch(error){
-    //         if (error instanceof TypeError){
-    //             console.error("Network shut down")
-    //         }
-    //         else{
-    //             console.log("error:"+error)
-    //         }
-    //     }
-        
-    // }
     useEffect(()=>{
          fetchTables(setTables)
     },[])
