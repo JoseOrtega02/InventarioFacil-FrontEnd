97d09a465c7be496b013f0ca73557b2e57ddd6a6
jose <ortegajoseweb02@gmail.com>
2024-08-19 10:16:15 -0300

    adding a getItems method[refactor necessary]

--- src/pages/Item/Item.tsx
+++ src/pages/Item/Item.tsx
@@ -1,17 +1,30 @@
+import { useParams } from "react-router-dom"
 import AddItem from "./component/AddItem"
+import { useEffect, useState } from "react"
+import { getItems } from "./utils/itemUtils"
 
+export interface Table{
+  tableId: string,
+  title: string,
+  items: object[]
+}
 
 function Item() {
-    const tittle = "example"
-    const tableId="aaaa"
+    const {id}=useParams() 
+    const [table,setTable]= useState<Table>({
+      tableId:"",
+      title:"",
+      items:[]
+    })
+    useEffect(()=>{
+      getItems(id,setTable)
+    },[])
   return (
     <>
-        <h1>{tittle}</h1>
-        <h2>Id:{tableId}</h2>
+        <h1>{table?.title}</h1>
+        <h2>Id:{id}</h2>
         <h2>table</h2>
         <AddItem/>
-
-
     </>
   )
 }
--- src/pages/Item/utils/itemUtils.ts
+++ src/pages/Item/utils/itemUtils.ts
@@ -1,4 +1,5 @@
 import { handleFetchErrors } from "../../../utils/utils";
+import { Table } from "../Item";
 import { AddItemType } from "../YupSchemas/ItemYupSchema";
 import Cookies from "js-cookie";
 const backendURL = import.meta.env.VITE_BACKEND_URL
@@ -23,3 +24,23 @@ export const postItems = async(values:AddItemType[],tableId:string | undefined) 
         }
     })
 }
+export const getItems = async (id:string | undefined,setTable:React.Dispatch<React.SetStateAction<Table>>) =>{
+    const csrfToken = Cookies.get("x-csrf-token")?.toString()
+    const bodyReq= JSON.stringify({tableId:id})
+    await fetch(backendURL + "/table/item",{method:"GET",body:bodyReq,headers:{
+         'Content-Type': 'application/json',
+        "x-csrf-token":csrfToken || ""
+    },credentials:"include"})
+    .then(handleFetchErrors)
+    .then(res=>res.json())
+    .then(data=>setTable(data))
+    .catch(error=>{
+        if(error instanceof TypeError){
+            console.error("error Network: " +error)
+        }else{
+            console.error("Error: "+error)
+        }
+    })
+}
+
+
