fd6d65be73edb299e7773b80c48f92214d3a1b37
jose <ortegajoseweb02@gmail.com>
2024-08-20 14:43:56 -0300

    fixing table get Item page

--- src/pages/Item/Item.tsx
+++ src/pages/Item/Item.tsx
@@ -3,28 +3,29 @@ import AddItem from "./component/AddItem"
 import { useEffect, useState } from "react"
 import { getItems } from "./utils/itemUtils"
 
-export interface Table{
+export interface Table {
   tableId: string,
-  title: string,
+  tableName: string,
   items: object[]
 }
 
 function Item() {
-    const {id}=useParams() 
-    const [table,setTable]= useState<Table>({
-      tableId:"",
-      title:"",
-      items:[]
-    })
-    useEffect(()=>{
-      getItems(id,setTable)
-    },[])
+  const { id } = useParams()
+  const [table, setTable] = useState<Table>({
+    tableId: "",
+    tableName: "",
+    items: []
+  })
+  useEffect(() => {
+    getItems(id, setTable)
+  }, [])
+  console.log(table)
   return (
     <>
-        <h1>{table?.title}</h1>
-        <h2>Id:{id}</h2>
-        <h2>table</h2>
-        <AddItem/>
+      <h1>{table?.tableName}</h1>
+      <h2>Id:{id}</h2>
+      <h2>table</h2>
+      <AddItem />
     </>
   )
 }
