c9f872ae54bfb48b4c3aedfc73bf06ba30bf9103
jose <ortegajoseweb02@gmail.com>
2024-10-30 20:32:34 -0300

    adding a logout button

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,6 @@
-import { logOutFetch } from "../../pages/Login/utils/loginUtils"
-import React, { useState } from 'react'
-import { Menu, X } from 'lucide-react'
+import { useState } from 'react'
 import { Menubar } from 'primereact/menubar';
-import { Label } from "@radix-ui/react-dropdown-menu";
-import { Avatar } from "primereact/avatar";
-import { InputText } from "primereact/inputtext";
+import UserComponent from "./userComponent";
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
 
@@ -35,14 +31,9 @@ export default function Navbar() {
     }
   ]
   const start = <img alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" height="40" className="mr-2"></img>;
-  const end = (
-    <div className="flex align-items-center gap-2">
-      <Avatar image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png" shape="circle" />
-    </div>
-  );
   return (<div className="card">
 
-    <Menubar model={items} start={start} end={end} />
+    <Menubar model={items} start={start} end={UserComponent} />
   </div>
   )
 }
