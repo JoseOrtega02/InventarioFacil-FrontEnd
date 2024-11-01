9f37e2a7891d1ce4dbb0e0dad252e8e5499b4a56
jose <ortegajoseweb02@gmail.com>
2024-10-31 23:32:22 -0300

    adding styles to a login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,44 +2,51 @@ import { ErrorMessage, Field, Form, Formik, } from "formik"
 import { userSchemaLogin } from "./schemaYup/UserSchema";
 import { loginFetch } from "./utils/loginUtils";
 import { getCRSFToken, getUser } from "../../utils/utils";
+import { InputText } from "primereact/inputtext";
+import { Card } from "primereact/card";
 
 
 function Login() {
-    return (<>
-      <Formik
-        initialValues={{ userName: '', password: '' }}
-        validationSchema={userSchemaLogin}
-        onSubmit={async (values, { setSubmitting }) => {
-          await getCRSFToken()
-          await loginFetch(values)
-          setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <div>
+  return (<>
+    <Formik
+      initialValues={{ userName: '', password: '' }}
+      validationSchema={userSchemaLogin}
+      onSubmit={async (values, { setSubmitting }) => {
+        await getCRSFToken()
+        await loginFetch(values)
+        setSubmitting(false);
+      }}
+    >
+      {({ isSubmitting, values, handleChange, handleBlur }) => (
+        <Form className="">
+          <Card title="Simple Card" className="">
+
+            <div className="flex flex-column gap-2 ">
               <label htmlFor="userName">Username: </label>
-              <Field type="text" name="userName" id="userName" />
-              <ErrorMessage name="userName" component="div" className="error" />
+              <InputText id="userName" name="userName" aria-describedby="username-help" value={values.userName} onChange={handleChange} onBlur={handleBlur} />
+              <ErrorMessage name="userName" component="small" className="p-error" />
             </div>
-  
-            <div>
+
+            <div className="flex flex-column gap-2 ">
               <label htmlFor="password">Password: </label>
-              <Field type="password" name="password" id="password" />
-              <ErrorMessage name="password" component="div" className="error" />
+              <InputText type="password" name="password" id="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
+              <ErrorMessage name="password" component="small" className="p-error" />
             </div>
-  
+
             <button type="submit" disabled={isSubmitting}>
               Log In
             </button>
-          </Form>
-        )}
-      </Formik>
-      <button onClick={()=> {const user= getUser()
-        console.log(user)
-      }}>checkUser</button>
-      </>
-    );
-  }
+
+          </Card>
+        </Form>
+      )}
+    </Formik>
+    <button onClick={() => {
+      const user = getUser()
+      console.log(user)
+    }}>checkUser</button>
+  </>
+  );
+}
 
 export default Login
