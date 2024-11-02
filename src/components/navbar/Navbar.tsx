7675ce25d7f28ae37b13058093ae05ee1f43c857
jose <ortegajoseweb02@gmail.com>
2024-11-02 17:13:37 -0300

    adding a logo in a component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import { NavbarContainer } from "./coponents/containers";
+import Navlinks from "./coponents/Navlinks";
 
 export default function Navbar() {
 
 
   return (<NavbarContainer>
+    <Navlinks />
     <h1>navbar</h1>
 
   </NavbarContainer>
---
+++ src/components/navbar/coponents/Navlinks.tsx
@@ -0,0 +1,10 @@
+import svg from "@/logo.svg"
+function Navlinks() {
+  return (
+    <div>
+      <img src={svg} />
+    </div>
+  )
+}
+
+export default Navlinks
