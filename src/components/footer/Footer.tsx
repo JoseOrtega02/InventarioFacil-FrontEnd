ac89fa3afa8ce7ab39c94c2df30d347e86ba420b
jose <ortegajoseweb02@gmail.com>
2024-11-03 20:07:45 -0300

    footer for desktop complete

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,18 @@
-
+import { styled } from "@/styled-system/jsx"
+const FooterContainer = styled.footer`
+display:flex;
+justify-content: start;
+padding:24px;
+height: 90px;
+background-color: token(colors.color2);
+align-items:center;
+color: token(colors.color1);
+`
 function Footer() {
   return (
-    <>footer</>
+    <FooterContainer>
+      <h4>Created by: <a href="https://www.linkedin.com/in/jose-manuel-ortega-gordillo" rel='noopener noreferrer' target="_blank" style={{ textDecoration: "underline" }}>Jose Ortega</a></h4>
+    </FooterContainer>
   )
 }
 
