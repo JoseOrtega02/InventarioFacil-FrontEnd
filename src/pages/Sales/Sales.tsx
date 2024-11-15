1170e97a9c61f24cf93d4728961f7230b60b9e71
jose <ortegajoseweb02@gmail.com>
2024-11-14 22:35:28 -0300

    adding the charts library

--- src/pages/Sales/Sales.tsx
+++ src/pages/Sales/Sales.tsx
@@ -1,6 +1,31 @@
 import { useEffect, useState } from "react"
 import { getSales } from "./Utils/SalesUtils"
 import DeleteSale from "./Components/DeleteSale"
+import { ReactECharts, ReactEChartsProps } from "./Components/ReactECharts"
+const option: ReactEChartsProps["option"] = {
+  tooltip: {
+    trigger: "axis",
+    axisPointer: {
+      type: "shadow",
+    },
+  },
+  legend: {
+    data: ["Owned", "Financed"],
+  },
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: 'bar'
+    }
+  ]
+}
 function Sales() {
   const [data, setData] = useState()
   useEffect(() => {
@@ -12,6 +37,7 @@ function Sales() {
     <div>
 
       <h1>Yor Sales</h1>
+      <ReactECharts option={option} />
       <DeleteSale />
     </div>
   )
