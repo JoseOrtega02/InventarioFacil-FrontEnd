7ce1f39a8aa7eefc01b3d9c078a19d5f3d7372bd
jose <ortegajoseweb02@gmail.com>
2024-11-20 16:33:02 -0300

    styling the make sale page

--- src/pages/MakeSale/MakeSale.tsx
+++ src/pages/MakeSale/MakeSale.tsx
@@ -1,7 +1,20 @@
 import { ItemInterface, useSaleStore } from '../zustand/itemsSalesState'
 import { postSale } from '../Sales/Utils/SalesUtils'
 import ItemComponent from './components/ItemComponent'
-
+import { styled } from '@/styled-system/jsx'
+import { TitleBlack } from '@/src/components/styledComponents/Texts'
+import { PrimaryButton } from '@/src/components/styledComponents/Buttons'
+const ContainerKart = styled.div`
+display:flex;
+flex-direction:column;
+gap:12px;
+align-items:center;
+margin-left:auto;
+margin-right:auto;
+width:60%;
+justify-content:center;
+margin-top:24px;
+`
 function MakeSale() {
   const items = useSaleStore(state => state.items)
   const fakeItems = [
@@ -16,15 +29,19 @@ function MakeSale() {
   function returnPayloadSale(items: ItemInterface[]) {
     return { saleItems: items }
   }
-  return (
-    <div>
+
+  return (<>
+   <ContainerKart>
+       <TitleBlack>Your kart</TitleBlack>
       {fakeItems?.map((item) => <ItemComponent item={item} />)}
-      <button onClick={() => {
+      <PrimaryButton  onClick={() => {
         //const payload = returnPayloadSale(items)
         ////////////////postSale(payload)
         alert("sale succesfull")
-      }}>Make the sale</button>
-    </div>
+      }}>Make the sale</PrimaryButton>
+    </ContainerKart>
+  </>
+   
   )
 }
 
--- src/pages/MakeSale/components/ItemComponent.tsx
+++ src/pages/MakeSale/components/ItemComponent.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { styled } from '@/styled-system/jsx'
+import svg from "@/public/options-vertical-svgrepo-com.svg"
 interface props {
   item: {
     name: string,
@@ -8,14 +9,34 @@ interface props {
     tableId: string
   }
 }
+
+const ItemContainer= styled.div`
+display:flex;
+align-items:center;
+flex-direction:row;
+gap:32px;
+border: 2px solid token(colors.color2);
+border-radius: 24px;
+padding: 12px 24px;
+`
+
+const OptionsButton= styled.button`
+background-color: token(colors.color2);
+padding: 8px 10px;
+border-radius:24px;
+`
+const Icon = styled.img`
+width:18px;
+height: 20px;
+`
 function ItemComponent({ item }: props) {
   return (
-    <div>
+    <ItemContainer>
       <h4>{item.name}</h4>
-      <h4>{item.price}</h4>
-      <h4>{item.quantity}</h4>
-      <button>more options</button>
-    </div>
+      <h4>Units: {item.quantity}</h4>
+      <h4>$ {item.price}</h4>
+      <OptionsButton><Icon src={svg}/></OptionsButton>
+    </ItemContainer>
   )
 }
 
